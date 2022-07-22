e187f8b93806ff64a460c150e612a67dec83ef8f
rullyafrizal <rullyafrizal4@gmail.com>
2022-07-22 10:16:25 +0700

    initialized

---
+++ src/main/java/com/rully/spring/reactor/LearnSpringReactorApplication.java
@@ -0,0 +1,13 @@
+package com.rully.spring.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringReactorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rully/spring/reactor/LearnSpringReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rully.spring.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
